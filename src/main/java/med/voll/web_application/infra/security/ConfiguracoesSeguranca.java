2e3b8fba8e19ca38f0dfbc5e2b789f90e263ef3a
Marcelo Yoshikazu <marcelo_yoshikazu@hotmail.com>
2025-02-26 20:33:13 -0300

    Criação da tabela Usuarios e utilização a tabela de usuarios para logar no sistema

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -8,21 +8,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
-
-    @Bean
-    public UserDetailsService dadosUsuariosCadastrados(){
-        //Construção de usuarios
-        UserDetails usuario1 = User.builder().username("joao@gmail.com").password("{noop}joao123").build();
-        UserDetails usuario2 = User.builder().username("maria@gmail.com").password("{noop}maria123").build();
-        UserDetails usuario3 = User.builder().username("iasmin@email.com").password("{noop}iasmin123").build();
-        return new InMemoryUserDetailsManager(usuario1,usuario2,usuario3);
-    }
 
     @Bean
     public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception{
@@ -36,5 +29,10 @@ public class ConfiguracoesSeguranca {
                 .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim").alwaysRemember(true))
                 .csrf(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder codificadorSenha(){
+        return new BCryptPasswordEncoder();
     }
 }
